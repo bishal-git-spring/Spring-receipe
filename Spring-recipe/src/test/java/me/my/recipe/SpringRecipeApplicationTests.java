1eed26123549e1219dc579da77b6cd6d22bba998
bishal.shrestha <bishal.shrestha@CIO54037-17.stone.ne.gov>
2020-03-19 12:31:00 -0500

    initial push

---
+++ Spring-recipe/src/test/java/me/my/recipe/SpringRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.my.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
